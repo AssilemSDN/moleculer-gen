d7195320b3e0c3e82e1801d1a31ff48ed03a3678
Melissa <albusdumbledore4269@gail.com>
2025-11-20 10:56:07 +0100

    feat(init-project): modularize docker services and fix service structure

--- src/generators/initProject/generate-modules.js
+++ src/generators/initProject/generate-modules.js
@@ -3,31 +3,67 @@
 */
 import { writeFile, writeYAML } from '../../utils/fs-helpers.js'
 import path from 'path'
-import { ConvertModuleToDockerAndEnv } from './convert-module-to-docker-and-env.js'
+import { renderTemplateToFile } from '../../utils/render-template.js'
+
+const writeDockerService = async (service, serviceName, projectDir) => {
+  const dockerServicePath = path.join(projectDir, `docker/services/${serviceName}.yaml`)
+  const doc = {
+    services: service
+  }
+  writeYAML(dockerServicePath, doc)
+}
+
+const writeFiles = async (templates, templateDir, projectDir) => {
+  console.log(templates)
+  templates?.map(template =>
+    renderTemplateToFile(path.join(templateDir, template.templatePath), path.join(projectDir, template.outputPath), template.data))
+}
 
 /**
  * Generate `docker-compose.yml` and `.env.example` for a list of modules.
  * @async
  * @param {Array<ModuleDefinition>} modules - List of modules to include
- * @param {string} outputDir - Directory where files will be generated
+ * @param {string} projectDir - Directory where files will be generated
  * @returns {Promise<void>}
  */
-export const generateModules = async (modules, outputDir) => {
-  const services = {}
+export const generateModules = async (modules, templateDir, projectDir) => {
   const envLines = []
 
+  // Generate module files and build env content
   for (const module of modules) {
-    const { service, envLines: lines } = ConvertModuleToDockerAndEnv(module)
-    Object.assign(services, service)
+    const service = {
+      [module.docker.serviceName]: {
+        image: module.docker.image,
+        container_name: module.docker.container_name,
+        environment: module.docker.environment,
+        networks: module.docker.networks,
+        volumes: module.docker.volumes,
+        ports: module.docker.ports,
+        command: module.docker.command,
+        depends_on: module.docker.depends_on,
+        labels: module.docker.labels,
+        env_file: module.docker.env_file,
+        security_opt: module.docker.security_opt,
+        restart: module.docker.restart
+      }
+    }
+    writeDockerService(service, module.meta.key, projectDir)
+    if (module.templates?.length) {
+      writeFiles(module.templates, templateDir, projectDir)
+    }
+    const moduleEnvLines = Object.entries(module.env).map(([k, v]) => `${k}=${v}`)
+    // Build .env file lines content
     envLines.push(`# ${module.meta.name}`)
-    envLines.push(...lines)
+    envLines.push(...moduleEnvLines)
     envLines.push('')
   }
 
-  // --- write docker-compose.yml ---
-  const dockerComposeDoc = {
+  // --- Prepare files to generate
+
+  const envContent = envLines.join('\n')
+  const dockerComposeMinimalContent = {
     // version: '3.9',
-    services,
+    services: {},
     networks: {
       publique: {
         name: 'publique',
@@ -41,14 +77,16 @@ export const generateModules = async (modules, outputDir) => {
     },
     volumes: { db_data: {} }
   }
-  const envContent = envLines.join('\n')
-  const envExamplePath = path.join(outputDir, '.env.example')
-  const envDevPath = path.join(outputDir, '.env.dev')
-  const composePath = path.join(outputDir, 'docker-compose.yaml')
 
-  await Promise.all([
-    writeYAML(composePath, dockerComposeDoc),
-    writeFile(envExamplePath, envContent),
-    writeFile(envDevPath, envContent)
-  ])
+  const envExamplePath = path.join(projectDir, '.env.example')
+  const envDevPath = path.join(projectDir, '.env.dev')
+  const composePath = path.join(projectDir, 'docker-compose.yaml')
+
+  // --- Generate files
+
+  // Write minimal docker compose
+  writeYAML(composePath, dockerComposeMinimalContent)
+  // Write env files
+  writeFile(envExamplePath, envContent)
+  writeFile(envDevPath, envContent)
 }
--- src/generators/initProject/generate.js
+++ src/generators/initProject/generate.js
@@ -37,15 +37,18 @@ export const generate = async ({
   // Ensure the output directory is clean
   await ensureEmptyDir(projectDir)
   await mkdirp(path.join(projectDir, '.moleculer-gen/'))
+  await mkdirp(path.join(projectDir, 'docker/services/'))
 
   // Copy base template
   await copyDir(path.join(templateDir, 'base'), projectDir)
 
   // Run generation tasks in parallel
   await Promise.all([
+    // Generate moleculer-gen config file
     generateConfig(answers, projectDir),
+    // Generate package.json project file
     generatePackageJson(answers.projectNameSanitized, projectDir, context),
-    generateModules(modules, projectDir),
+    // Generate readme.md file
     renderTemplateToFile(
       path.join(templateDir, 'README.mustache'),
       path.join(projectDir, 'README.md'),
@@ -54,6 +57,10 @@ export const generate = async ({
         database: answers.database,
         transporter: answers.transporter
       }
-    )
+    ),
+    // Generate minimal docker-compose.yml
+    // --- TODO ---
+    // Generate needed yml docker services, env, and files
+    generateModules(modules, templateDir, projectDir)
   ])
 }
