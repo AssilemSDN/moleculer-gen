902bbd2f86dd322e3f21cbdab4a963580d30b016
Melissa <albusdumbledore4269@gmail.com>
2026-04-05 19:19:14 +0200

    fix(cli): read version dynamically from package.json and use preAction hook for logger

--- bin/cli.js
+++ bin/cli.js
@@ -6,17 +6,21 @@
 import { program } from 'commander'
 import { applyLoggerLevel, registerGlobalOptions } from '../src/cli/options.js'
 import { registerCommands } from '../src/cli/commands.js'
+import { createRequire } from 'module'
+
+const require = createRequire(import.meta.url)
+const { version } = require('../package.json')
+
 
 // CLI definition
 program
   .name('moleculer-gen')
   .description('Moleculer project generator')
-  .version('0.0.1')
-
+  .version(version)
 // Options
 registerGlobalOptions(program)
 // init commands
 registerCommands(program)
+program.hook('preAction', (thisCommand) => applyLoggerLevel(thisCommand))
+
 program.parse()
-// Log levels
-applyLoggerLevel(program)
