392bbd45af09422d1e1e66614682ee1dd6480c6b
Melissa <albusdumbledore4269@gmail.com>
2026-08-20 17:11:20 +0200

    fix: use explicit extension in ESM import

--- tests/helpers/cli.js
+++ tests/helpers/cli.js
@@ -1,7 +1,7 @@
 import { execFile } from 'node:child_process'
 import { promisify } from 'node:util'
 
-import { cliPath } from './paths'
+import { cliPath } from './paths.js'
 
 const execFileAsync = promisify(execFile)
 
