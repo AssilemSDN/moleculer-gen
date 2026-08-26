b86cb045c631bfdbbe7928f99f5b97b9d37f3048
Melissa <pufferfish.bloop@proton.me>
2026-08-26 18:09:45 +0200

    fix(validate): Fix unexpected internal errors that were transformed into user errors

--- src/validators/project-validator.js
+++ src/validators/project-validator.js
@@ -2,6 +2,7 @@
   PATH /src/validators/project-validator.js
 */
 
+import { AppError } from '../errors/AppError.js'
 import { logger } from '../utils/logger.js'
 import { validateMoleculerConfig } from './validate-moleculer-config.js'
 
@@ -25,8 +26,13 @@ export const projectValidator = async (projectDir = process.cwd()) => {
     errors.push(...result.errors)
     warnings.push(...result.warnings)
   } catch (err) {
+    // Unexpected errors must reach safeRun and be reported as internal errors.
+    if (!(err instanceof AppError)) {
+      throw err
+    }
+    // User error.
     errors.push({
-      code: err.code ?? 'PROJECT_VALIDATION_ERROR',
+      code: err.code,
       message: err.message,
       details: err.details ?? null
     })
