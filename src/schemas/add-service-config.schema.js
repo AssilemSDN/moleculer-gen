fd7b7186962ab6341c31d2431d1fa46357241da4
Melissa <pufferfish.bloop@proton.me>
2026-08-26 16:30:43 +0200

    fix(config-schema): add .trim()

--- src/schemas/add-service-config.schema.js
+++ src/schemas/add-service-config.schema.js
@@ -1,16 +1,16 @@
 import { z } from 'zod'
 
 export const addServiceConfigSchema = z.object({
-  serviceName: z.string().min(1),
+  serviceName: z.string().trim().min(1),
   isCrud: z.boolean().optional(),
   exposeApi: z.boolean().optional(),
 
   serviceFileName: z.string().min(1).optional(),
-  serviceDirectoryName: z.string().min(1).optional(),
+  serviceDirectoryName: z.string().trim().min(1).optional(),
 
-  modelFileName: z.string().min(1).optional(),
-  modelName: z.string().min(1).optional(),
-  modelVariableName: z.string().min(1).optional(),
-  collectionName: z.string().min(1).optional(),
-  schemaName: z.string().min(1).optional()
+  modelFileName: z.string().trim().min(1).optional(),
+  modelName: z.string().trim().min(1).optional(),
+  modelVariableName: z.string().trim().min(1).optional(),
+  collectionName: z.string().trim().min(1).optional(),
+  schemaName: z.string().trim().min(1).optional()
 })
