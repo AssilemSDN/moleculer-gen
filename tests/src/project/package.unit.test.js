10e32e625d772df68d303b4a912a996c9a4825d2
Melissa <pufferfish.bloop@proton.me>
2026-08-30 17:43:51 +0200

    test: add package.ts unit test

---
+++ tests/src/project/package.unit.test.js
@@ -0,0 +1,27 @@
+import { describe, expect, it } from 'vitest'
+
+import {
+  basePackage
+} from '../../../dist/project/package.js'
+
+describe('basePackage', () => {
+  it('contains only project-wide runtime dependencies', () => {
+    expect(basePackage.dependencies).toMatchObject({
+      moleculer: '0.15.1',
+      'moleculer-db': '0.9.0',
+      'moleculer-web': '0.11.0'
+    })
+  })
+
+  it('does not contain module-specific dependencies', () => {
+    expect(basePackage.dependencies).not.toHaveProperty(
+      'nats'
+    )
+    expect(basePackage.dependencies).not.toHaveProperty(
+      'mongoose'
+    )
+    expect(basePackage.dependencies).not.toHaveProperty(
+      'moleculer-db-adapter-mongoose'
+    )
+  })
+})
